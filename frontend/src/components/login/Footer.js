02f1d2e216cdbed907d9fa4eb510ac79d440558d
Danny Nunez <114783191+dnunez22@users.noreply.github.com>
2023-02-22 19:50:31 -0500

    login update

--- frontend/src/components/login/Footer.js
+++ frontend/src/components/login/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="login_footer">
       <div className="login_footer_wrap">
-        <Link to="/">English(UK)</Link>
+        {/* <Link to="/">English(UK)</Link>
         <Link to="/">Français(FR)</Link>
         <Link to="/">العربية</Link>
         <Link to="/">ⵜⴰⵎⴰⵣⵉⵖⵜ</Link>
@@ -14,14 +14,14 @@ export default function Footer() {
         <Link to="/">Português (Brasil)</Link>
         <Link to="/">हिन्दी</Link>
         <Link to="/">中文(简体)</Link>
-        <Link to="/">日本語</Link>
+        <Link to="/">日本語</Link> */}
         <Link to="/" className="footer_square">
           <i className="plus_icon"></i>
         </Link>
       </div>
       <div className="footer_splitter"></div>
       <div className="login_footer_wrap">
-        <Link to="/">Sign Up</Link>
+        {/* <Link to="/">Sign Up</Link>
         <Link to="/">Log in</Link>
         <Link to="/">Messenger</Link>
         <Link to="/">Facebook Lite</Link>
@@ -45,7 +45,7 @@ export default function Footer() {
         <Link to="/">Developers</Link>
         <Link to="/">Careers</Link>
         <Link to="/">Privacy</Link>
-        <Link to="/">Cookies</Link>
+        <Link to="/">Cookies</Link> */}
         <Link to="/">
           AdChoices
           <i className="adChoices_icon"></i>
@@ -55,7 +55,7 @@ export default function Footer() {
       </div>
       <div className="login_footer_wrap">
         <Link to="/" style={{ fontSize: "12px", marginTop: "10px" }}>
-          Meta © 2022
+          Sociolly © 2023
         </Link>
       </div>
     </footer>
