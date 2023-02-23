88745c46ca334178e39228a2f57e43613a4599a1
Danny Nunez <114783191+dnunez22@users.noreply.github.com>
2023-02-22 21:40:21 -0500

    updates

--- frontend/src/components/home/right/index.js
+++ frontend/src/components/home/right/index.js
@@ -1,42 +1,17 @@
 import { Dots, NewRoom, Search } from "../../../svg";
-import { useEffect, useReducer } from "react";
-import { friendspage } from "../../../functions/reducers";
-import { getFriendsPageInfos } from "../../../functions/user";
-// import Contact from "./Contact";
-import sponsorscreen from "../../../svg/tide.gif";
-
+import Contact from "./Contact";
 import "./style.css";
-import Card from "./Card";
 export default function RightHome({ user }) {
   const color = "#65676b";
-
-  const [{ loading, error, data }, dispatch] = useReducer(friendspage, {
-    loading: false,
-    data: {},
-    error: "",
-  });
-  useEffect(() => {
-    getData();
-  }, []);
-  const getData = async () => {
-    dispatch({ type: "FRIENDS_REQUEST" });
-    const data = await getFriendsPageInfos(user.token);
-    if (data.status === "ok") {
-      dispatch({ type: "FRIENDS_SUCCESS", payload: data.data });
-    } else {
-      dispatch({ type: "FRIENDS_ERROR", payload: data.data });
-    }
-  };
-
   return (
     <div className="right_home">
-      <div className="heading">Friends</div>
-
+      <div className="heading">Sponsored</div>
       <div className="splitter1"></div>
       <div className="contacts_wrap">
         <div className="contacts_header">
+          <div className="contacts_header_left">Contacts</div>
           <div className="contacts_header_right">
-            {/* <div className="contact_circle hover1">
+            <div className="contact_circle hover1">
               <NewRoom color={color} />
             </div>
             <div className="contact_circle hover1">
@@ -44,21 +19,11 @@ export default function RightHome({ user }) {
             </div>
             <div className="contact_circle hover1">
               <Dots color={color} />
-            </div> */}
+            </div>
           </div>
         </div>
         <div className="contacts_list">
-          {/* <Contact user={user} /> */}
-
-          {data.friends &&
-            data.friends.map((user) => (
-              <Card
-                userr={user}
-                key={user._id}
-                type="friends"
-                getData={getData}
-              />
-            ))}
+          <Contact user={user} />
         </div>
       </div>
     </div>
